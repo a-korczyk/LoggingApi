3d80b909950830cd8bd4bffd08068e3f0a7dbad8
a-korczyk <269985293+a-korczyk@users.noreply.github.com>
2026-06-15 18:09:37 +0200

    Add JWT service

---
+++ src/LoggingApi.Infrastructure/Services/JwtProvider.cs
@@ -0,0 +1,40 @@
+using System.Security.Claims;
+using System.Text;
+using LoggingApi.Application.Abstractions.Services;
+using LoggingApi.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
+
+namespace LoggingApi.Infrastructure.Services;
+
+/// <summary>
+/// Implementation of <see cref="IJwtProvider"/>
+/// </summary>
+public class JwtProvider(IConfiguration configuration) : IJwtProvider
+{
+    public string CreateToken(User user)
+    {
+        string secretKey = configuration["Jwt:Secret"];
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity([
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            ]),
+            Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+            SigningCredentials = credentials,
+            Issuer = configuration["Jwt:Issuer"],
+            Audience = configuration["Jwt:Audience"],
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        string token = handler.CreateToken(tokenDescriptor);
+        return token;
+    }
+}
