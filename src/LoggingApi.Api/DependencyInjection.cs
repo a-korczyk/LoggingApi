421eb472eaa3952a3dd7fa56bb09375c9d0788e5
a-korczyk <269985293+a-korczyk@users.noreply.github.com>
2026-07-20 14:20:16 +0200

    feat: reject access tokens belonging to recently deleted users

--- src/LoggingApi.Api/DependencyInjection.cs
+++ src/LoggingApi.Api/DependencyInjection.cs
@@ -1,4 +1,7 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
+using LoggingApi.Infrastructure.Services;
 using LoggingApi.Infrastructure.Services.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -31,7 +34,7 @@ public static class DependencyInjection
         var accessTokenOptions = configuration
             .GetSection(AccessTokenOptions.SectionName)
             .Get<AccessTokenOptions>() ?? throw new InvalidOperationException("Access token configuration not found.");
-        
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
@@ -39,16 +42,41 @@ public static class DependencyInjection
                 {
                     ValidIssuer = accessTokenOptions.Issuer,
                     ValidateIssuer = true,
-            
+
                     ValidAudience = accessTokenOptions.Audience,
                     ValidateAudience = true,
-            
+
                     IssuerSigningKey = new SymmetricSecurityKey(
                         Encoding.UTF8.GetBytes(accessTokenOptions.Secret)),
-            
+
                     ValidateIssuerSigningKey = true,
-            
+
                     ValidateLifetime = true,
+                };
+
+                // Check if token has required claims and make sure
+                // the user identifier points to an existing account (not deleted)
+                options.Events.OnTokenValidated = async context =>
+                {
+                    var cacheService = 
+                        context.HttpContext.RequestServices.GetRequiredService<ICacheService>();
+
+                    Console.WriteLine(context.Principal?.Claims);
+                    
+                    var userId = 
+                        context.Principal?.FindFirstValue(ClaimTypes.NameIdentifier);
+                    
+                    var userEmail =
+                        context.Principal?.FindFirstValue(ClaimTypes.Email);
+                    
+                    if (userId is null || userEmail is null)
+                        context.Fail("Invalid token.");
+
+                    var wasRecentlyDeletedCacheResult = await cacheService.GetAsync<bool>(
+                        $"deleted:users:{userId}");
+                    
+                    if (wasRecentlyDeletedCacheResult.Exists)
+                        context.Fail("User no longer exists.");
                 };
             });
         
